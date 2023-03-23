013ca716fe433e0d31ebca1b364a740a3ba9a898
massif-py <111177666+massif-py@users.noreply.github.com>
2023-03-23 14:18:48 -0600

    Checking path-validity

---
+++ checkingPathValidity.py
@@ -0,0 +1,9 @@
+import os
+
+print(os.path.exists('C:\\Users\\gaabr'))
+print(os.path.exists('C:\\Users\\massif'))
+print(os.path.isdir('C:\\Windows\\System32'))
+print(os.path.isfile('C:\\Windows\\System32'))
+print(os.path.isdir('C:\\Windows\\System32\\calc.exe'))
+print(os.path.isfile('C:\\Windows\\System32\\calc.exe'))
+print(os.path.exists('D:\\'))
